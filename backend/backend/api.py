d5cecfe26dccd939835b8a022e8b9485d4715a43
Danil Maximov <jokjokjokerface@gmail.com>
2020-03-17 17:53:23 +0300

    Create backend app

---
+++ backend/backend/api.py
@@ -0,0 +1,5 @@
+import graphene
+
+from backend.todo_list.schema import Query, Mutation
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
---
+++ backend/todo_list/models.py
@@ -0,0 +1,40 @@
+from datetime import timedelta
+
+from django.db import models
+from django.utils import timezone
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+def get_due_date():
+    """By default, you are given one day to complete the task."""
+    return timezone.now() + timedelta(days=1)
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=250)
+    text = models.TextField(blank=True)
+    created_date = models.DateField(auto_now_add=True)
+    due_date = models.DateField(default=get_due_date)
+    category = models.ForeignKey(
+        Category,
+        related_name='todo_list',
+        on_delete=models.PROTECT,
+    )
+    done = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+    def __str__(self):
+        return self.title
